3f085c2cc6d60e97dd53c3f9b586ca10f848d6b2
François Voron <francois.voron@nereo.fr>
2017-01-04 14:36:40 +0100

    Generate project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPlanningApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularPlanningApp
+ */
+angular.module('angularPlanningApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
